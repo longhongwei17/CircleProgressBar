d24836a074ab356548490d651d46861dc9b71df5
龙红伟 <longhongwei@longhongweideMacBook-Pro.local>
2017-04-12 14:53:41 +0800

    En

---
+++ CircleProgressBar/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  CircleProgressBar
+//
+//  Created by 龙红伟 on 2017/4/12.
+//  Copyright © 2017年 appleDeveloper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ CircleProgressBar/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  CircleProgressBar
+//
+//  Created by 龙红伟 on 2017/4/12.
+//  Copyright © 2017年 appleDeveloper. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
